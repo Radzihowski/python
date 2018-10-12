625a77ed5129e8e0437f27989dfc09ba0b996fca
Radzihowski <A.Radzihovskiy@gmail.com>
2018-10-12 21:28:08 +0100

    Add files via upload

---
+++ addToInventory.py
@@ -0,0 +1,6 @@
+def addToInventory(inventory, addedItems):
+    code
+
+inv = {"gold coin": 42, "rope": 1}
+dragonLoot = ["gold coin", "dagger", "gold coin", "gold coin", "ruby"]
+inv = addToInventory(inv)
