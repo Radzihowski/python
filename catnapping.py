9c605dd28695d663546a5cef62f202fd785460e9
radzihowski <a.radzihovskiy@gmail.com>
2018-10-31 22:00:00 +0000

    next update

--- catnapping.py
+++ catnapping.py
@@ -1,6 +1,2 @@
-print('''Dear Alice,
-
-Eve's cat has been arrested for catnapping, cat burglary, and extortion.
-
-Sincerely,
-Bob''')
+spam = "Hello world!"
+print(spam[lentght(spam)-5:lenght(spam)])
