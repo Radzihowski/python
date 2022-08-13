18cc3905bfa3aee79cba02bf43274de1db7f7642
Oleksandr Radzikhovskyi <a.radzihovskiy@gmail.com>
2022-08-13 10:39:44 +0100

    Exercise 2 from 6.2

---
+++ realpython.com/ch04-strings-and-strings-methods/greet.py
@@ -0,0 +1,8 @@
+def greet(name):
+    """Takes one string parameter called name and display Hello name"""
+    print(f"Hello {name}!")
+
+greet("Ada")
+greet("Grace")
+greet("Albert")
+greet("Marie")
