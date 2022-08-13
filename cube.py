eed1db2966ff7888887a403de293641a6716050b
Oleksandr Radzikhovskyi <a.radzihovskiy@gmail.com>
2022-08-13 10:26:45 +0100

    # 6.2 - Write Your Own Functions # Exercise 1

---
+++ cube.py
@@ -0,0 +1,11 @@
+def cube(num):
+    """Return the value of number raised to the third power"""
+    return(pow(num, 3))
+
+print(f"0 cuber is {cube(0)}")
+print(f"1 cubed is {cube(1)}")
+print(f"2 cubed is {cube(2)}")
+print(f"3 cubed is {cube(3)}")
+print(f"4 cubed is {cube(4)}")
+print(f"5 cubed is {cube(5)}")
+print(f"6 cubed is {cube(6)}")
