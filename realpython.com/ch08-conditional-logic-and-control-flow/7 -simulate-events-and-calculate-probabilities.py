e5cdb4317acd355d5b8e5c3cea5242bd1b073ab7
DESKTOP-JPEJ3R0\ARadz <a.radzihovskiy@gmail.com>
2023-07-22 13:26:01 +0100

    coin_flip for chapter 8.7

---
+++ realpython.com/ch08-conditional-logic-and-control-flow/7 -simulate-events-and-calculate-probabilities.py
@@ -0,0 +1,22 @@
+# 8.7 - Simulate Events and Calculate Probabilities
+
+# Example 1
+import random
+
+def coin_flip():
+    if random.randint(0, 1) == 0:
+        return "heads"
+    else:
+        return "tails"
+
+heads_tally = 0
+tails_tally = 0
+
+for trial in range (100000):
+    if coin_flip() == "heads":
+        heads_tally += 1
+    else:
+        tails_tally += 1
+
+ratio = (heads_tally / tails_tally)
+print(f"The ratio of heads to tails is {ratio}")
