21a172f3908b3da131997be4cd1b3affee6604bf
DESKTOP-JPEJ3R0\ARadz <a.radzihovskiy@gmail.com>
2023-07-23 06:46:33 +0100

    Solution for two exercises for chapter 8.7

--- realpython.com/ch08-conditional-logic-and-control-flow/7 -simulate-events-and-calculate-probabilities.py
+++ realpython.com/ch08-conditional-logic-and-control-flow/7 -simulate-events-and-calculate-probabilities.py
@@ -41,4 +41,19 @@ for trial in range(10000):
 ratio = heads_tally / tails_tally
 print(f"The ratio of heads of tails is {ratio}")
 
+# Exercise 1
+# Write a function called roll() that uses randint() to simulate rolling a fair die by returning a random integer between 1 and 6
 
+def roll():
+    return random.randint(1,6)
+
+# Exercise 2
+# Write a program that simulates ten thousands rolls of a fair die and displays the average number rolled.
+num_rolls = 10_000
+total = 0
+
+for trial in range(num_rolls):
+    total += roll()
+
+average = total / num_rolls
+print(f"The average number rolled is {average}")
