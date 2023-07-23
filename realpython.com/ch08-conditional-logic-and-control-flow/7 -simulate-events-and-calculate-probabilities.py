b1d431a4a32f9894e4bbaf891501fb0a04fcdf32
DESKTOP-JPEJ3R0\ARadz <a.radzihovskiy@gmail.com>
2023-07-23 06:18:38 +0100

    Example of unfair_coin_flip for chapter 8.7

--- realpython.com/ch08-conditional-logic-and-control-flow/7 -simulate-events-and-calculate-probabilities.py
+++ realpython.com/ch08-conditional-logic-and-control-flow/7 -simulate-events-and-calculate-probabilities.py
@@ -20,3 +20,25 @@ for trial in range (100000):
 
 ratio = (heads_tally / tails_tally)
 print(f"The ratio of heads to tails is {ratio}")
+
+# Example 2
+
+def unfair_coin_flip(probability_of_tails):
+    if random.random() < probability_of_tails:
+        return "tails"
+    else:
+        return "heads"
+
+heads_tally = 0
+tails_tally = 0
+
+
+for trial in range(10000):
+    if unfair_coin_flip(.7) == "heads":
+        heads_tally += 1
+    else:
+        tails_tally += 1
+ratio = heads_tally / tails_tally
+print(f"The ratio of heads of tails is {ratio}")
+
+
