d801e7c5a1d8fc68b38b38e967ecf39c2d1601fe
DESKTOP-JPEJ3R0\ARadz <a.radzihovskiy@gmail.com>
2023-07-29 20:29:41 +0100

    Solution 1-4 for 9.1 chapter

---
+++ realpython.com/ch09-tuples-lists-and-dictionaries/1-tuples-are-immutable-sequences.py
@@ -0,0 +1,24 @@
+# 9.1 Exercises
+
+# 1. Create a tuple ...
+cardinal_number = ("first", "second", "third")
+print(cardinal_number)
+
+# 2. Using index notation and print() ...
+print(cardinal_number[1])
+
+# 3. In a single line of code, unpack the values in cardinal_numbers into three new strings names position1, position2
+# position3. Then print each value on separate line
+position1, position2, position3 = cardinal_number[0], cardinal_number[1], cardinal_number[2]
+print(position1)
+print(position2)
+print(position3)
+
+# 4. Using tuple() and a string literal, create a tuple called my_name that contains the letters of your name
+myname = ('m', 'a', 'r', 'k', 't', 'e', 'p', 'l', 'e', 'y')
+print(myname)
+
+# 5. Check whether the character 'x' is in my_name using the in keyword
+
+# 6. Create a new tuple containing all but the first letter in my_name using slice notation.
+
