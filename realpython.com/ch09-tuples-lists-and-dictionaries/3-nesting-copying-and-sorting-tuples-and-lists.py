880d51ab2ce4b31114e77116464c355c90aadcd6
DESKTOP-JPEJ3R0\ARadz <a.radzihovskiy@gmail.com>
2023-08-01 15:49:25 +0100

    Solutions for 9.3 chapter

--- realpython.com/ch09-tuples-lists-and-dictionaries/3-nesting-copying-and-sorting-tuples-and-lists.py
+++ realpython.com/ch09-tuples-lists-and-dictionaries/3-nesting-copying-and-sorting-tuples-and-lists.py
@@ -7,17 +7,20 @@ print(data)
 # 2. Write a for loop that loops over data and prints the sum of each nested tuple. The output should look like this:
 # Row 1 sum: 3
 # Row 2 sum: 4
+i = 1
 for k in data:
-    print(f"Row {k} sum: {data}")
-
+    print(f"Row {i} sum: {sum(k)}")
+    i += 1
 
 # 3. Create the list [4, 3, 2, 1] and assign it to the variable numbers
-
+numbers = [4, 3, 2, 1]
+print(numbers)
 
 # 4. crate a copy of the numbers list using the [:] slice notation
-
+copy_number = numbers[:]
+print(copy_number)
 
 # 5. Sort the numbers list in numerical order using .sort()
-
-
+numbers.sort()
+print(numbers)
 
