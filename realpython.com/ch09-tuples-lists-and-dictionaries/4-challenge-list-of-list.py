06b50ea93ea35e1ffa5a49897fe5bd30ada97919
DESKTOP-JPEJ3R0\ARadz <a.radzihovskiy@gmail.com>
2023-08-06 13:35:28 +0100

    Solutions for 9.4 chapter

--- realpython.com/ch09-tuples-lists-and-dictionaries/4-challenge-list-of-list.py
+++ realpython.com/ch09-tuples-lists-and-dictionaries/4-challenge-list-of-list.py
@@ -2,6 +2,15 @@
 
 # Write a program that contains the following lists of lists:
 
+universities = [
+    ['California Institute of Technology', 2175, 37704],
+    ['Harvard', 19627, 39849],
+    ['Massachusetts Institute of Technology', 10566, 40732],
+    ['Princeton', 7802, 37000],
+    ['Rice', 5879, 35551],
+    ['Stanford', 19535, 40569],
+    ['Yale', 11701, 40500]
+]
 
 def enrollment_stats(list_of_universities):
     student_enrollment_values = []
@@ -17,23 +26,13 @@ def mean(values):
 
 def median(values):
     values.sort()
-    if len(values) / 2 == 1:
+    if len(values) % 2 == 1:
         center_index = int(len(values)/2)
         return values[center_index]
     else:
         left_center_index = (len(values) - 1) / 2
         right_center_index = (len(values) + 1) / 2
         return mean([values[left_center_index], values[right_center_index]])
-
-universities = [
-    ['California Institute of Technology', 2175, 37704],
-    ['Harvard', 19627, 39849],
-    ['Massachusetts Institute of Technology', 10566, 40732],
-    ['Princeton', 7802, 37000],
-    ['Rice', 5879, 35551],
-    ['Stanford', 19535, 40569],
-    ['Yale', 11701, 40500]
-]
 
 totals = enrollment_stats(universities)
 
