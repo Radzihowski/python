551e39ec84bd185f21126bfd3b153dd554bd2148
DESKTOP-JPEJ3R0\ARadz <a.radzihovskiy@gmail.com>
2023-08-08 23:39:02 +0100

    Solutions for 9.5 chapter

--- realpython.com/ch09-tuples-lists-and-dictionaries/5-challenge-wax-poetic.py
+++ realpython.com/ch09-tuples-lists-and-dictionaries/5-challenge-wax-poetic.py
@@ -14,16 +14,40 @@ def make_a_poem():
     noun1 = random.choice(nouns)
     noun2 = random.choice(nouns)
     noun3 = random.choice(nouns)
+    # This will allow not repeatable nouns
+    while noun1 == noun2:
+        noun2 = random.choice(nouns)
+    while noun1 == noun3 or noun2 == noun3:
+        noun3 = random.choice(nouns)
+
     verb1 = random.choice(verbs)
     verb2 = random.choice(verbs)
     verb3 = random.choice(verbs)
+    while verb1 == verb2:
+        verb2 = random.choice(verbs)
+    while verb1 == verb3 or verb2 == verb3:
+        verb3 = random.choice(verbs)
+
     adj1 = random.choice(adjectives)
     adj2 = random.choice(adjectives)
     adj3 = random.choice(adjectives)
+    while adj1 == adj2:
+        adj2 = random.choice(adjectives)
+    while adj1 == adj3 or adj2 == adj3:
+        adj3 = random.choice(adjectives)
+
     prep1 = random.choice(prepositions)
     prep2 = random.choice(prepositions)
+    while prep1 == prep2:
+        prep2 = random.choice(prepositions)
+
     adverb1 = random.choice(adverbs)
-    article = random.choice(["A", "An"])
+
+    # Logic to use An/A articles
+    if "aeiou".find(adj1[0]) != -1:
+        article = "An"
+    else:
+        article = "A"
 
     poem = (
         f"{article} {adj1} {noun1}\n\n"
