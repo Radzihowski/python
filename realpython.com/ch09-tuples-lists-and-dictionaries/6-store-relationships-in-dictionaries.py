a66b35d5479b420df6c03f1b15b74dba844439dc
DESKTOP-JPEJ3R0\ARadz <a.radzihovskiy@gmail.com>
2023-08-11 23:53:03 +0100

    Tasks for chapter 9.6

---
+++ realpython.com/ch09-tuples-lists-and-dictionaries/6-store-relationships-in-dictionaries.py
@@ -0,0 +1,15 @@
+# 9.6 Exercises
+
+# 1. Create an empty dictionary named captains
+
+# 2. Using square bracket notation, enter the following data into the dictionary one item at a time
+
+# 3. Write two if statement that check if "Enterprise" and "Discovery" exist as keys in dictionary. Set their values to
+# "unknown" if the key does not exist
+
+# 4. Write for a loop to display the ship and captain name contained in the dictionary. For example,
+# the output should look something like this: The Enterprise is captained by Picard.
+
+# 5. Delete "Discovery" from the dictionary
+
+# 6. Bonus: Make the same dictionary by using dict() and passing in the initial values when you first create the dictionary.
