cf57f0606f606fe0280b1b82b2d7f8b8b2affea6
DESKTOP-JPEJ3R0\ARadz <a.radzihovskiy@gmail.com>
2023-08-22 22:41:43 +0100

    Task for 9.9 chapter

---
+++ realpython.com/ch09-tuples-lists-and-dictionaries/9-challenge-cats-with-hats.py
@@ -0,0 +1,6 @@
+# You have a hundred cats
+# One day, you decided to arrange all your cats in a giant circle. Initially, none of your cats has a hat on. You walk
+# around the circle a hundred times, always starting with the first cat. Each time you stop at a cat, you check if it
+# has a hat on. If it doesn't, then you put a hat on it. If it does, then you take the hat off.
+
+def get_cats_dith_hats():
