0c33d1a3584184cc0d377fbf59467ee7cf7c2e19
DESKTOP-JPEJ3R0\ARadz <a.radzihovskiy@gmail.com>
2023-09-01 21:11:19 +0100

    First exercise for 10.3

---
+++ realpython.com/ch10-object-oriented-programming/3-inherit-from-other-classes.py
@@ -0,0 +1,16 @@
+# 10.3 Exercises
+
+# 1. Create a GoldenRetriver class that inherits from the Dog class.
+class Dof:
+    species = "Canis familiaris"
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def __init__(self):
+        return f"{self.name} is {self.age} years old"
+
+    def __init__(self, sound):
+        return f"{self.name} says {sound}"
+
